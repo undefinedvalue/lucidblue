c72860de4cf128441dca7df39c8b66d4c138a601
Anna Bell <undefinedvalue@users.noreply.github.com>
2016-06-24 00:19:31 -0700

    Auto-include post dates

--- development/build_pipeline/100_compile_templates.py
+++ development/build_pipeline/100_compile_templates.py
@@ -2,13 +2,50 @@
 
 import os
 import shutil
+import subprocess
+import datetime
+import dateutil.parser
+from dateutil.tz import tzstr
+from subprocess import Popen, PIPE
 from jinja2 import Environment, FileSystemLoader
 from util import fileutils as futil
+
+def commitDate(path):
+  git = Popen(['git', 'log', '--format=%aI', '--reverse', path], stdout=PIPE)
+
+  post_date = datetime.datetime.now().isoformat()
+  for line in git.stdout:
+    post_date = line.strip()
+    break;
+
+  git.terminate()
+  git.wait()
+
+  return post_date
+
 
 def build(src_dir, dst_dir, opts):
   loader = FileSystemLoader(src_dir)
   env = Environment(auto_reload=False, trim_blocks=True, lstrip_blocks=True, loader=loader)
   env.globals['environment'] = opts.get('environment')
+
+  posts = []
+
+  for (root, dirs, files) in os.walk(os.path.join(src_dir, 'posts')):
+    for f in files:
+      if f.startswith('_'):
+        template_path = os.path.join(root, f)
+
+        post_date = commitDate(template_path)
+
+        date = dateutil.parser.parse(post_date).astimezone(tzstr("PST8PDT"))
+        pretty_date = date.strftime('%A, %B ') + str(date.day) +\
+            date.strftime(', %Y')
+
+        template_path = os.path.relpath(template_path, src_dir)
+        posts.append((post_date, pretty_date, template_path))
+
+  env.globals['post_templates'] = sorted(posts, reverse=True)
 
   # Render all files in the src_dir that have a ".j2" extension
   for (src_path, dst_path) in futil.pairwalk(src_dir, dst_dir):
